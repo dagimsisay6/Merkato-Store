d1a883c77d847f52601c7809bfc91274bd96c5a7
Dagim <dagisis1224@gmail.com>
2026-07-10 20:42:45 +0100

    fix: use React.use() to unwrap params in admin product form

--- client/app/admin/products/[id]/page.js
+++ client/app/admin/products/[id]/page.js
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter, use } from "next/navigation";
+import { useEffect, useState, use } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { ArrowLeft, Save } from "lucide-react";
 
